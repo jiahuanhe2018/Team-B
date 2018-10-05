8bde6184cd4af49ed0ad22179138eeca3ac213e8
shouyidaxin <lsx467695638@163.com>
2018-10-05 22:21:26 +0800

    Add files via upload
    update pos code

--- 李守鑫-第二课作业/blockchain/pos.go
+++ 李守鑫-第二课作业/blockchain/pos.go
@@ -14,22 +14,19 @@ import (
 	"math/rand"
 )
 
-type SimpleBlock struct{
-	singBlock Block
-}
 // validators keeps track of open validators and balances
 var Validators = make(map[string]int)
 //tmpblock
 var TempBlocks []Block
 
-var announcements  = make(chan []Block)
+var announcements = make(chan []Block)
 
-func pickWinner(){
+func pickWinner(rw *bufio.ReadWriter) {
 	time.Sleep(10 * time.Second)
 	mutex.Lock()
 	temp := TempBlocks
 	mutex.Unlock()
-	fmt.Print("pickWinner timer1111111111111111111111111111111,temp = \n",temp)
+	fmt.Print("pickWinner timer1111111111111111111111111111111,temp = \n", temp)
 	lotteryPool := []string{}
 	if len(temp) > 0 {
 		fmt.Print("tmp = 0\n")
@@ -57,11 +54,9 @@ func pickWinner(){
 				}
 			}
 
-			mutex.Lock()
-			TempBlocks = []Block{}
-			mutex.Unlock()
-		}
 
+		}
+		fmt.Print("lotteryPool = \n", len(lotteryPool))
 		// randomly pick winner from lottery pool
 		s := rand.NewSource(time.Now().Unix())
 		r := rand.New(s)
@@ -70,29 +65,44 @@ func pickWinner(){
 		// add block of winner to blockchain and let all the other nodes know
 		for _, block := range temp {
 			if block.Validator == lotteryWinner {
+
+				tmpBlockChain := BlockchainInstance.Blocks
 				mutex.Lock()
-				BlockchainInstance.Blocks = append(BlockchainInstance.Blocks, block)
+				tmpBlockChain = append(tmpBlockChain, block)
 				mutex.Unlock()
 
-				fmt.Printf("pickwinner success---%v\n",lotteryWinner)
-				announcements <- BlockchainInstance.Blocks
+				fmt.Printf("\x1b[32m pickwinner success---%v\x1b[0m\n", lotteryWinner)
+				//announcements <- tmpBlockChain
+				bytes, err := json.Marshal(tmpBlockChain)
+				if err != nil {
+					log.Println(err)
+				}
+				rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
+				rw.Flush()
 				break
 
 			}
 		}
+		mutex.Lock()
+		TempBlocks = []Block{}
+		mutex.Unlock()
 	}
 }
 
-//ReadDataByPow
+//ReadDataByPos
 func ReadDataByPos(rw *bufio.ReadWriter) {
 	fmt.Printf("0000000000000000\n")
 
 	go func() {
-		pickWinner()
+		for{
+			pickWinner(rw)
+		}
+
 	}()
 
 	go func() {
 		msg := <-announcements
+		fmt.Printf("update_msg,len=%d,  block= \n %v\n",len(msg),msg)
 		bytes, err := json.Marshal(msg)
 		if err != nil {
 			log.Println(err)
@@ -112,13 +122,15 @@ func ReadDataByPos(rw *bufio.ReadWriter) {
 			fmt.Printf("str= null\n")
 			return
 		}
+
 		if str != "\n" {
 
-			inputBlock := Block{0,"",0,"","",100,nil,nil,1,"",""}
+			inputBlock := Block{}
 			block := make([]Block, 0)
 			validator := make(map[string]int)
 
 			if err := json.Unmarshal([]byte(str), &validator); err != nil {
+				fmt.Printf("Unmarshal validator = bad\n")
 				//log.Println(err)
 			} else {
 
@@ -132,20 +144,23 @@ func ReadDataByPos(rw *bufio.ReadWriter) {
 			}
 
 			if err := json.Unmarshal([]byte(str), &inputBlock); err != nil {
-				//fmt.Printf("Unmarshal inputBlock = bad\n")
+				fmt.Printf("Unmarshal inputBlock = bad\n")
 				//log.Println(err)
 			} else {
 
+				mutex.Lock()
 				TempBlocks = append(TempBlocks, inputBlock)
-				fmt.Printf("Unmarshal block,tmpBlock = %v\n",TempBlocks)
+				mutex.Unlock()
+				fmt.Printf("Unmarshal block,tmpBlock = %v\n", TempBlocks)
 				continue
 			}
 
 			if err := json.Unmarshal([]byte(str), &block); err != nil {
-				//fmt.Printf("Unmarshal block = bad\n")
+				fmt.Printf("Unmarshal block = bad\n")
 				//log.Println(err)
 			} else {
-				fmt.Printf("33333333333333333333\n")
+				fmt.Printf("33333333333333333333,,new_block = %d block_len=%d\n",len(block),len(BlockchainInstance.Blocks))
+				fmt.Printf("33333333333333333333,,new_block = \n%v \n",block)
 				mutex.Lock()
 				if len(block) > len(BlockchainInstance.Blocks) {
 					BlockchainInstance.Blocks = block
@@ -154,6 +169,10 @@ func ReadDataByPos(rw *bufio.ReadWriter) {
 
 						log.Fatal(err)
 					}
+					fmt.Printf("MarshalIndent =\n")
+					//mutex.Lock()
+					TempBlocks = []Block{}
+					//mutex.Unlock()
 					// Green console color: 	\x1b[32m
 					// Reset console color: 	\x1b[0m
 					fmt.Printf("\x1b[32m%s\x1b[0m> ", string(bytes))
@@ -171,18 +190,16 @@ func ReadDataByPos(rw *bufio.ReadWriter) {
 func WriteDataByPos(rw *bufio.ReadWriter) {
 	fmt.Printf("WriteData_000000000000000000000000\n")
 
-
 	go func() {
 		for {
 			time.Sleep(10 * time.Second)
 			fmt.Printf("WriteData_1111111111111111111\n")
-			mutex.Lock()
+			//mutex.Lock()
 			bytes, err := json.Marshal(BlockchainInstance.Blocks)
 			if err != nil {
 				log.Println(err)
 			}
-			mutex.Unlock()
-
+			//mutex.Unlock()
 			mutex.Lock()
 			rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
 			rw.Flush()
@@ -190,8 +207,23 @@ func WriteDataByPos(rw *bufio.ReadWriter) {
 
 		}
 	}()
-
-	fmt.Printf("WriteData_222222222222222222222\n")
+	//go func() {
+	//	for {
+	//		time.Sleep(5 * time.Second)
+	//		mutex.Lock()
+	//		bytes, err := json.Marshal(Validators)
+	//		if err != nil {
+	//			log.Println(err)
+	//		}
+	//		mutex.Unlock()
+	//
+	//		mutex.Lock()
+	//		rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
+	//		rw.Flush()
+	//		mutex.Unlock()
+	//
+	//	}
+	//}()
 	stdReader := bufio.NewReader(os.Stdin)
 
 	fmt.Print("\nEnter token balance: ")
@@ -218,36 +250,32 @@ func WriteDataByPos(rw *bufio.ReadWriter) {
 	}
 
 	rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
-	rw.Flush()
+	//rw.Flush()
 	mutex.Unlock()
 
 	for {
-	//	tempBlock := make([]Block, 0)
+		//	tempBlock := make([]Block, 0)
 
 		fmt.Print("\nEnter a new Result:")
 		result, err := stdReader.ReadString('\n')
-		fmt.Print("\n             00000000000000")
 		if err != nil {
 			delete(Validators, address)
 		}
-		fmt.Print("\n             11111111111111111")
 		result = strings.Replace(result, "\n", "", -1)
-		fmt.Print("\n             2222222222222222")
-		iResult, err := strconv.Atoi(balance)
+		iResult, err := strconv.Atoi(result)
 		mutex.Lock()
-		fmt.Print("\n             33333333333333-len = ",len(BlockchainInstance.Blocks))
+		fmt.Print("\n             33333333333333-len = ", len(BlockchainInstance.Blocks))
 		oldLastBlockIndex := BlockchainInstance.Blocks[len(BlockchainInstance.Blocks)-1]
 		mutex.Unlock()
-	//	newblock := SimpleBlock;
+
 		newBlock := GenerateBlockByPos(oldLastBlockIndex, iResult, address)
-
 		//tempBlock = append(tempBlock, newBlock)
-
+		fmt.Print("\n    newBlock.result = \n", newBlock.Result)
 		bytes, err := json.Marshal(newBlock)
 		if err != nil {
 			log.Println(err)
 		}
-		fmt.Print("\n             33333333333333-newBlock = ",newBlock)
+		fmt.Print("\n             33333333333333-newBlock = ", newBlock)
 		mutex.Lock()
 		rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
 		rw.Flush()
@@ -262,13 +290,13 @@ func GenerateBlockByPos(oldBlock Block, Result int, str string) Block {
 
 	t := time.Now()
 
-	newBlock.Index 			= oldBlock.Index + 1
-	newBlock.Timestamp 		= t.String()
-	newBlock.Result 		= Result
-	newBlock.PrevHash 		= oldBlock.Hash
-	newBlock.Hash 			= CalculateHash(newBlock)
-	newBlock.Difficultly 	= difficultly
-	newBlock.Validator 		= str
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	newBlock.Result = Result
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = CalculateHash(newBlock)
+	newBlock.Difficultly = difficultly
+	newBlock.Validator = str
 	return newBlock
 }
 
